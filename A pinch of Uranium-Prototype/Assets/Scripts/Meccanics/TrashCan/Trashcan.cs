c35d817a816fd848b4594b1a946d8e19b9928701
Tremorlives <tremorlives@yahoo.com>
2024-03-30 07:09:39 +0100

    Strange Commit

--- A pinch of Uranium-Prototype/Assets/Scripts/Meccanics/TrashCan/Trashcan.cs
+++ A pinch of Uranium-Prototype/Assets/Scripts/Meccanics/TrashCan/Trashcan.cs
@@ -3,13 +3,19 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 [RequireComponent(typeof(Collider))]
 public class Trashcan : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] List<string> validIDs;
     [SerializeField] int itemsNeeded;
     [SerializeField] TextMeshProUGUI displayText;
+
+    [Header("On Completed")]
+    [SerializeField] GameObject completeVFX;
+    [SerializeField] GameObject itemDrop;
     List<GameObject> validItemList;
     int currentItemAmount = 0;
     private void Awake()
@@ -51,6 +57,16 @@ public class Trashcan : MonoBehaviour
         else
         {
             displayText.text = "FULL!";
+            // Drop the object and summon an effect
+            CompleteTrashCan();
         }
+    }
+
+    private void CompleteTrashCan()
+    {
+        Instantiate(completeVFX, transform.position, Quaternion.identity);
+        GameObject drop = Instantiate(itemDrop, transform.position, Quaternion.identity);
+        Destroy(this);
+        Debug.Log("Destroyed Trash Can!");
     }
 }
